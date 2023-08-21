ebc8aaf771c2373a56a0268338f8cc57676967ee
maguozhen <839363837@qq.com>
2023-08-21 14:39:18 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -36,11 +36,13 @@ async fn main() {
   Registrar::new().start();
   RouteSyncer::new().start();
 
-  HttpServer::new(move || App::new().route("/ws", web::get().to(index)))
-    .backlog(100000)
-    .bind(format!("{}:{}", "0.0.0.0", CONFIG.http_port))
-    .unwrap()
-    .run()
-    .await
-    .unwrap();
+  HttpServer::new(move || {
+    App::new().wrap(middleware::Logger::default()).route("/ws", web::get().to(index))
+  })
+  .backlog(100000)
+  .bind(format!("{}:{}", "0.0.0.0", CONFIG.http_port))
+  .unwrap()
+  .run()
+  .await
+  .unwrap();
 }
